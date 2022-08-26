746f3e8ad19a52639f6389568f8b4bf533ada6f5
Vasu Kaushik <vasu.kaushik@tcs.com>
2022-08-26 16:52:32 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const date=new Date().getFullYear();
+function Footer(){
+    return (
+        <Footer>
+         <p>
+          Copyright ⓒ {date}
+          </p>
+          </Footer>
+    )
+}
+
+export default Footer;
